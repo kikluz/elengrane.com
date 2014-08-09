1c0b3e3793d873d1541240bf92ff789c79b20b2b
Enrique Rivas Rivera <savir81@hotmail.com>
2014-08-09 11:38:05 -0700

    working o switchers buttons

--- source/assets/javascripts/components/slide-show.js
+++ source/assets/javascripts/components/slide-show.js
@@ -11,12 +11,38 @@ $(function() {
 });
 
 // ------------------------------------------------------------
-// style switch jquery
+// style switcher jquery
 // ------------------------------------------------------------
-$(document).ready(function(){
-
-  $('#switcher-large').on('click', function(){
-    $('body').addClass('large')
+$(document).ready(function() {
+  $('#switcher h3').click(function() {
+    $('#switcher button').toggleClass('hidden');
   });
 });
+// ------------------------------------------------------------
+// style switcher jquery: hover
+// ------------------------------------------------------------
+$(document).ready(function(){
+  $('#switcher h3').hover(function(){
+    $(this).addClass('hover');}, function(){
+      $(this).removeClass('hover');
+  });
+});
+
+ 
+$(document).ready(function() {
+  $('#switcher-default').addClass('selected');
+ 
+  $('#switcher button').click(function() {
+    var bodyClass = this.id.split('-')[1];
+ 
+    $('body').removeClass().addClass(bodyClass);
+ 
+    $('#switcher button').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
+
+
+
+
 
