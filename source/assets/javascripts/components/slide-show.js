4a9c9722e31636bd08fe3bd3a6d2a58ea06e0d0a
Enrique Rivas Rivera <savir81@hotmail.com>
2014-08-16 14:58:53 -0700

    working on jq

--- source/assets/javascripts/components/slide-show.js
+++ source/assets/javascripts/components/slide-show.js
@@ -24,31 +24,11 @@ setInterval(function() {
 // ------------------------------------------------------------
 // style switcher jquery
 // ------------------------------------------------------------
-
-  $('#switcher h3').hover(function() {
-    $(this).addClass('hover');
-  }, function() {
-    $(this).removeClass('hover');
-  });
+$('#switcher h3').hover(function() {
+  $(this).addClass('hover');
+}, function() {
+  $(this).removeClass('hover');
 });
- 
-$(document).ready(function() {
-  $('#switcher').click(function(event) {
-    $('#switcher button').toggleClass('hidden');
-  });
-});
- 
-$(document).ready(function() {
-  $('#switcher-default').addClass('selected');
- 
-  $('#switcher button').click(function() {
-    var bodyClass = this.id.split('-')[1];
- 
-    $('body').removeClass().addClass(bodyClass);
- 
-    $('#switcher button').removeClass('selected');
-    $(this).addClass('selected');
-  });
 });
 
 
