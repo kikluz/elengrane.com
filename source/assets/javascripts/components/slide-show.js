8bfa27fed2d351a1be5797e49ab5f5533358c716
Enrique Rivas Rivera <savir81@hotmail.com>
2014-09-26 12:41:47 -0700

    modified js

--- source/assets/javascripts/components/slide-show.js
+++ source/assets/javascripts/components/slide-show.js
@@ -39,23 +39,38 @@ $(document).ready(function() {
       default:
         num = parseFloat(defaultSize);
     }
-    $speech.css('fontSize', num + 'px');
+    $speech.animate({fontSize: num + 'px'}, 'slow');
   });
-    var $firstPara = $('p').eq(1);
-    $firstPara.hide();
-    $('a.more').click(function(event)
-    {
-      /// tagglling visibility
-      event.preventDefault();
-      $firstPara.slideToggle('slow');
-      var $link = $(this);
-      if ($link.text() == 'read more') {
-        $link.text('read less');
-      } else {
-        $link.text('read ,more');
-      }
-    });
+ 
+  var $firstPara = $('p').eq(1);
+  $firstPara.hide();
+  $('a.more').click(function(event) {
+    event.preventDefault();
+    $firstPara.animate({
+      opacity: 'toggle',
+      height: 'toggle'
+    }, 'slow');
+    var $link = $(this);
+    if ($link.text() == 'read more') {
+      $link.text('read less');
+    } else {
+      $link.text('read more');
+    }
+  });
+ 
+  $('div.label').click(function() {
+    var paraWidth = $('div.speech p').outerWidth();
+    var $switcher = $(this).parent();
+    var switcherWidth = $switcher.outerWidth();
+    $switcher.css({
+      position: 'relative'
+    }).animate({left: paraWidth - switcherWidth}, 'slow')
+    .animate({height: '+=20px'}, 'slow')
+    .animate({borderWidth: '5px'}, 'slow');
+  });
 });
+
+
 
 
 // ------------------------------------------------------------
