779df860e90fd76245d1e5f37a18bef0fa65fafc
kikluz <savir81@hotmail.com>
2015-09-02 16:12:47 -0700

    add new code for el engrane fade in

--- source/assets/javascripts/components/scroll.js
+++ source/assets/javascripts/components/scroll.js
@@ -30,3 +30,34 @@
 jQuery(function(){	
   jQuery.mark.jump();
 });
+
+
+$(document).ready(function() {
+  var firstLoad = true;
+  show();
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+    firstLoad = false;
+    show();
+  });
+  
+  function show () {
+    /* Check the location of each desired element */
+    $('.toShow').each( function(i){
+            
+      var bottom_of_object = $(this).position().top + $(this).outerHeight() - ($(this).outerHeight() / 2);
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+
+        $(this).removeClass('toShow');
+        if (firstLoad == false) {
+          $(this).addClass('animated bounceInUp');
+        }
+      }  
+
+    });
+  }
+    
+});
